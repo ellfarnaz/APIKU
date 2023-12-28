28b9aa06f1b26da9914866d82777a2d9724ea57b
user <user@192.168.1.14>
2023-12-29 01:19:16 +0700

    initial commit

--- routes/group.js
+++ routes/group.js
@@ -6,16 +6,19 @@ const groupData = [
     id: 172,
     npm: "5210411172",
     name: "FILLAH ABY PRAMUDYA",
+    angatan: "2021",
   },
   {
     id: 2,
     npm: "5210411167",
     name: "AGNESTA LINDA SARI",
+    angatan: "2021",
   },
   {
     id: 3,
     npm: "5210411174",
     name: "VERATINA FRIDAYANTI",
+    angatan: "2021",
   },
 ];
 
